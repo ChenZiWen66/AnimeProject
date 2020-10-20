635779147d06bc6f202904b7de5bc26315767787
chenziwen <401030526@qq.com>
2020-10-20 10:05:24 +0800

    1.规划了页面的各个内容的大致位置 2.编写了页面顶部的部分

---
+++ web/src/components/PageTop.vue
@@ -0,0 +1,25 @@
+<template>
+    <!--  顶部标签  -->
+    <div class="row">
+        <!--    顶部图片    -->
+        <div class="col-md-2">
+            <img src="../assets/xiaolan.jpg" alt="顶部左侧图片" style="height: 42px;width: 182px">
+        </div>
+        <div class="col-md-6"></div>
+        <div class="col-md-4">
+            <p>
+                这是三个标签
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navigate"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,57 @@
+<template>
+    <!--  主界面  -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <!--        顶部导航栏        -->
+                <div class="row">
+                    <div class="col-md-12" style="background-color: antiquewhite">
+                        <PageTop/>
+                    </div>
+                </div>
+                <!--       顶部标签栏         -->
+                <div class="row">
+                    <div class="col-md-12" style="background-color: #42b983">
+                        顶部标签
+                    </div>
+                </div>
+                <!--       轮播栏            -->
+                <div class="row">
+                    <div class="col-md-12" style="background-color: cornsilk">
+                        轮播栏
+                    </div>
+                </div>
+                <!--       tag标签栏            -->
+                <div class="row">
+                    <div class="col-md-12" style="background-color: aquamarine">
+                        标签栏
+                    </div>
+                </div>
+                <!--       内容栏            -->
+                <div class="row">
+                    <div class="col-md-9" style="background-color: chartreuse">
+                        内容栏
+                    </div>
+                    <div class="col-md-3" style="background-color: cornflowerblue">
+                        每日更新列表
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import PageTop from "../components/PageTop";
+
+    export default {
+        name: "Main",
+        components: {
+            PageTop
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

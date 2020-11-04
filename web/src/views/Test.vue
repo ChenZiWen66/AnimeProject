414b269e76db3f5f6c2c036029e6e1459a0b2f2a
chenziwen <401030526@qq.com>
2020-11-04 16:34:24 +0800

    1.删掉了两个没有用的ace模板HTML文件 2.准备了将代码进行改造，使用js将一些经常使用的方法拿出来，使代码更加舒服

--- web/src/views/Test.vue
+++ web/src/views/Test.vue
@@ -1,26 +1,16 @@
 <template>
     <div class="container-fluid">
-        <div class="col-md-12">
-            <AliPlayerManage ref="player1"/>
-        </div>
-        <div class="col-md-12">
-            <button @click="click_btn"/>
-        </div>
-
+        <button @click="click_btn"/>
     </div>
 </template>
 
 <script>
-    import AliPlayerManage from "../components/Management/AliPlayerManage";
-
+import OSSFileUtils from "../components/utils/OSSFileUtils";
     export default {
         name: "Test",
-        components: {
-            AliPlayerManage
-        },
         methods: {
-            click_btn(){
-                this.$refs.player1.addPlayer('标题',"https://czwhub.oss-cn-shanghai.aliyuncs.com/xiaolan.jpg","https://czwhub.oss-cn-shanghai.aliyuncs.com/SEVENS%20014..mp4");
+            click_btn() {
+                OSSFileUtils.sayHello("陈子文");
             }
         }
     }
